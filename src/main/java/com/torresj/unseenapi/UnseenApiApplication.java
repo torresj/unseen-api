820294cc4d4f6bad86a271b1702594a73cc7196d
Jaime Torres <jtbenavente@gmail.com>
2022-12-21 09:05:42 +0100

    First commit

---
+++ src/main/java/com/torresj/unseenapi/UnseenApiApplication.java
@@ -0,0 +1,13 @@
+package com.torresj.unseenapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UnseenApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UnseenApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/torresj/unseenapi/UnseenApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.torresj.unseenapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UnseenApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
